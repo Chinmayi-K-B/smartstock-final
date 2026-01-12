625135b3b58843955f3498c34dacc417dbfb77d9
Chinmayi K B <chinmayikantesha.badigera@gmail.com>
2026-01-12 14:04:27 +0530

    Force update helper URL

--- Frontend/inventory_management_system/src/helper.js
+++ Frontend/inventory_management_system/src/helper.js
@@ -1,8 +1,2 @@
-// UNCOMMENT the line you want to use
-
-// OPTION 1: Localhost (Use this for coding on your laptop)
 // export const BASE_URL = "http://localhost:3001";
-
-// OPTION 2: Render Backend (Use this when deploying)
-export const BASE_URL = "https://smartstock-backend-x2vv.onrender.com"; 
-// ^ We will update this link in a few minutes!
+export const BASE_URL = "https://smartstock-backend.onrender.com";
